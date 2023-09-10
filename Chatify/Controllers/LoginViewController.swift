4f1ff81dfd53d13399173e2f5b339729119fb2eb
AmrMohamad <amr.mohamed124124@gmail.com>
2023-09-10 23:41:59 +0300

    Add Register Button and
    It is responsible for representing the UI and the logic of UI

--- Chatify/Controllers/LoginViewController.swift
+++ Chatify/Controllers/LoginViewController.swift
@@ -8,9 +8,92 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    var inputsContainer: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.cornerRadius = 12
+        view.layer.masksToBounds = true
+        view.backgroundColor = .white
+        
+        return view
+    }()
     
+    let registerButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Register", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 25)
+        button.backgroundColor = UIColor(red: 80/255, green: 101/255, blue: 161/255, alpha: 1)
+        button.layer.cornerRadius = 12
+        return button
+    }()
+    let nameTextField: UITextField = {
+        let tf = UITextField()
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.placeholder = "Name"
+        tf.font = UIFont.systemFont(ofSize: 22, weight: .regular)
+        return tf
+    }()
+    let emailTextField: UITextField = {
+        let tf = UITextField()
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.placeholder = "Email"
+        tf.font = UIFont.systemFont(ofSize: 22, weight: .regular)
+        return tf
+    }()
+    let passwordTextField: UITextField = {
+        let tf = UITextField()
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.placeholder = "Password"
+        tf.font = UIFont.systemFont(ofSize: 22, weight: .regular)
+        tf.isSecureTextEntry = true
+        return tf
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = UIColor(red: 61/255, green: 91/255, blue: 151/255, alpha: 1)
+        
+        view.addSubview(inputsContainer)
+        setupInputsContainerConstraints()
+        view.addSubview(registerButton)
+        setupRegisterButtonConstraints()
     }
+    
+    func setupInputsContainerConstraints(){
+        // Postion of InputsContainer
+        inputsContainer.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        inputsContainer.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        // Size of InputsContainer
+        inputsContainer.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 1/4).isActive = true
+        inputsContainer.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85).isActive = true
+        let textFieldsStack = UIStackView(
+            arrangedSubviews: [
+                nameTextField,
+                emailTextField,
+                passwordTextField
+            ]
+        )
+        textFieldsStack.translatesAutoresizingMaskIntoConstraints = false
+        textFieldsStack.axis         = .vertical
+        textFieldsStack.alignment    = .fill
+        textFieldsStack.distribution = .fillEqually
+        textFieldsStack.spacing      = 0
+        inputsContainer.addSubview(textFieldsStack)
+        textFieldsStack.centerXAnchor.constraint(equalTo: inputsContainer.centerXAnchor).isActive = true
+        textFieldsStack.centerYAnchor.constraint(equalTo: inputsContainer.centerYAnchor).isActive = true
+        textFieldsStack.topAnchor.constraint(equalTo: inputsContainer.topAnchor, constant: 0).isActive = true
+        textFieldsStack.bottomAnchor.constraint(equalTo: inputsContainer.bottomAnchor, constant: 0).isActive = true
+        textFieldsStack.leadingAnchor.constraint(equalTo: inputsContainer.leadingAnchor, constant: 2).isActive = true
+        textFieldsStack.trailingAnchor.constraint(equalTo: inputsContainer.trailingAnchor, constant: -2).isActive = true
+        nameTextField.heightAnchor.constraint(equalTo: inputsContainer.heightAnchor, multiplier: 1/3).isActive = true
+        
+    }
+    func setupRegisterButtonConstraints(){
+        registerButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        registerButton.topAnchor.constraint(equalTo: inputsContainer.bottomAnchor, constant: 12).isActive = true
+        registerButton.widthAnchor.constraint(equalTo: inputsContainer.widthAnchor).isActive = true
+        registerButton.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 1/11).isActive = true
+    }
+    
 }
