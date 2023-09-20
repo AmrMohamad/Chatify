3d79215726fabc2ce88fbba4b4a81b1e3abe6d5c
AmrMohamad <amr.mohamed124124@gmail.com>
2023-09-20 15:39:45 +0300

    Add the name of user to navigation bar as a title of VC

--- Chatify/Controllers/MainViewController.swift
+++ Chatify/Controllers/MainViewController.swift
@@ -12,6 +12,7 @@ import FirebaseCore
 
 class MainViewController: UITableViewController {
 
+    let db = Firestore.firestore()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -21,11 +22,32 @@ class MainViewController: UITableViewController {
             target: self,
             action: #selector(handeleLogOut)
         )
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            image: UIImage(systemName: "plus.message"),
+            style: .plain,
+            target: self,
+            action: #selector(addNewMessage)
+        )
+        navigationController?.navigationBar.prefersLargeTitles = true
+        checkIfUserIsLogin()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        checkIfUserIsLogin()
+    }
+    
+    func checkIfUserIsLogin(){
         if Auth.auth().currentUser?.uid == nil {
             perform(#selector(handeleLogOut), with: nil, afterDelay: 0)
+        } else {
+            let uid = Auth.auth().currentUser!.uid
+            db.collection("users").document(uid).getDocument { snapshot, error in
+                if let userData = snapshot?.data() {
+                    self.title = userData["name"] as? String
+                }
+            }
         }
     }
-
 
     @objc func handeleLogOut(){
         do{
@@ -36,6 +58,10 @@ class MainViewController: UITableViewController {
         let loginVC = LoginViewController()
         loginVC.modalPresentationStyle = .fullScreen
         present(loginVC, animated: true)
+    }
+    
+    @objc func addNewMessage(){
+        
     }
 }
 
---
+++ Chatify/Controllers/NewMeesageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NewMeesageViewController.swift
+//  Chatify
+//
+//  Created by Amr Mohamad on 20/09/2023.
+//
+
+import UIKit
+
+class NewMeesageViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
